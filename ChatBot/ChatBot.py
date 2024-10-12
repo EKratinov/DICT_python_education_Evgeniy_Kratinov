0a84b417b8c5d569a7b26cd56bd698e930aee2af
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-12 14:59:27 +0300

    ChatBot 2-st stage

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -2,5 +2,15 @@ def greet(bot_name, birth_year):
     print(f"Hello! My name is {bot_name}.")
     print(f"I was created in {birth_year}.")
 
+def remind_name():
+    print("Please, remind me your name.")
+    name = input("> ")
+    print(f"What a great name you have, {name}!")
 
+
+
+
+
+# Виклик
 greet('DICT_Bot', 2024)
+remind_name()
