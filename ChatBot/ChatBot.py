2ae632bb23644bd9849e4501d919e0bd31d4a75a
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-12 15:17:35 +0300

    ChatBot 3-rd stage

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -7,10 +7,18 @@ def remind_name():
     name = input("> ")
     print(f"What a great name you have, {name}!")
 
-
+def guess_age():
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
+    rem3 = int(input("> "))
+    rem5 = int(input("> "))
+    rem7 = int(input("> "))
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    print(f"Your age is {age}; that's a good time to start programming!")
 
 
 
 # Виклик
-greet('DICT_Bot', 2024)
+greet('ChatBot', 2024)
 remind_name()
+guess_age()
