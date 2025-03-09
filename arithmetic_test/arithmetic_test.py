95dc5fb9becd73f270eefa8124858eacf9c9f63f
EKratinov <y.a.kratynov@student.itc.khai.edu>
2025-03-09 13:01:56 +0200

    Arithmetic test 2-nd stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,20 +1,28 @@
 import random
 
 
-a = random.randint(2, 9)
-b = random.randint(2, 9)
-operation = random.choice(['+', '-', '*'])
-
-
-print(f"{a} {operation} {b}")
-
-
-try:
-    user_answer = int(input("> "))
+def generate_task():
+    a = random.randint(2, 9)
+    b = random.randint(2, 9)
+    operation = random.choice(['+', '-', '*'])
     correct_answer = eval(f"{a} {operation} {b}")
-    if user_answer == correct_answer:
-        print("Right!")
-    else:
-        print("Wrong!")
-except ValueError:
-    print("Incorrect format.")
+    while True:
+        try:
+            user_input = input(f"{a} {operation} {b}\n> ")
+            user_answer = int(user_input)
+            if user_answer == correct_answer:
+                print("Right!")
+                return True
+            else:
+                print("Wrong!")
+                return False
+        except ValueError:
+            print("Incorrect format.")
+
+
+correct_count = 0
+for _ in range(5):
+    if generate_task():
+        correct_count += 1
+
+print(f"Your mark is {correct_count}/5.")
