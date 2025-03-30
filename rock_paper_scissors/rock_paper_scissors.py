90bfb5792610f116437afee22cab92ea812e5c57
EKratinov <y.a.kratynov@student.itc.khai.edu>
2025-03-30 15:36:10 +0300

    Rock, paper, scissors 3-rd stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,30 +1,29 @@
 import random
 
-
-def get_user_choice():
-    choice = input("Enter your choice (rock, scissors, paper): ").lower()
-    while choice not in ["rock", "scissors", "paper"]:
-        print("Invalid input. Please enter: rock, scissors, or paper.")
-        choice = input("Enter your choice: ").lower()
-    return choice
-
 def get_computer_choice():
     return random.choice(["rock", "scissors", "paper"])
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        return "It's a draw!"
+        return f"There is a draw ({computer_choice})"
     elif (user_choice == "rock" and computer_choice == "scissors") or \
          (user_choice == "scissors" and computer_choice == "paper") or \
          (user_choice == "paper" and computer_choice == "rock"):
-        return "Well done. The computer chose " + computer_choice + " and failed"
+        return f"Well done. The computer chose {computer_choice} and failed"
     else:
-        return "Sorry, but the computer chose " + computer_choice
+        return f"Sorry, but the computer chose {computer_choice}"
 
 def main():
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    print(determine_winner(user_choice, computer_choice))
+    while True:
+        user_choice = input("Enter your choice (rock, scissors, paper or exit): ").lower()
+        if user_choice == "exit":
+            print("Bye!")
+            break
+        elif user_choice in ["rock", "scissors", "paper"]:
+            computer_choice = get_computer_choice()
+            print(determine_winner(user_choice, computer_choice))
+        else:
+            print("Invalid input")
 
 if __name__ == "__main__":
     main()
