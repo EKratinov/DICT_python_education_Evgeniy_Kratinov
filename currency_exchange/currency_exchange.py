ed28a0391195654b86b773997f301930babea35c
EKratinov <y.a.kratynov@student.itc.khai.edu>
2025-04-20 16:45:46 +0300

    Currency exchange 3-rd stage

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,15 +1,20 @@
-def convert_multiple_currencies():
-    mycoins = float(input("Please, enter the number of mycoins you have: > "))
+import requests
 
-    exchange_rates = {
-        "ARS": 0.82,
-        "HNL": 0.17,
-        "AUD": 1.9622,
-        "MAD": 0.208
-    }
 
-    for currency, rate in exchange_rates.items():
-        converted_amount = round(mycoins * rate, 2)
-        print(f"I will get {converted_amount} {currency} from the sale of {mycoins} mycoins.")
+def get_exchange_rates(currency_code):
+    url = f"http://www.floatrates.com/daily/{currency_code}.json"
+    response = requests.get(url)
 
-convert_multiple_currencies()
+    if response.status_code == 200:
+        exchange_data = response.json()
+        if "usd" in exchange_data and "eur" in exchange_data:
+            print(f"Exchange rate to USD: {exchange_data['usd']['rate']:.2f}")
+            print(f"Exchange rate to EUR: {exchange_data['eur']['rate']:.2f}")
+        else:
+            print("No exchange rate data for USD or EUR.")
+    else:
+        print("Failed to retrieve exchange rates.")
+
+
+currency_code = input("Enter currency code: > ").lower()
+get_exchange_rates(currency_code)
