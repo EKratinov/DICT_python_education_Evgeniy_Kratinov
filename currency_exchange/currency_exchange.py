65bb5983b43b51ca52319522435e4ee44c345d68
EKratinov <y.a.kratynov@student.itc.khai.edu>
2025-04-20 17:17:01 +0300

    Currency exchange 4-th stage (modified)

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,41 +1,65 @@
 import requests
+import json
+import time
+import os
 
-cache = {}
+CACHE_FILE = "cache.json"
+CACHE_TTL = 300  # 5 минут (в секундах)
 
 
-def get_exchange_rate(base_currency, target_currency):
-    if not target_currency.isalpha() or len(target_currency) != 3:
-        print("Invalid currency code. Please enter a three-letter currency code.")
-        return None
+def load_cache():
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return data
+    return {}
 
-    if target_currency in cache:
-        print("Checking the cache...")
-        print("It is in the cache!")
-        return cache[target_currency]
 
-    print("Checking the cache...")
-    print("Sorry, but it is not in the cache!")
-    print(f"Fetching exchange rate for {target_currency}...")
+def save_cache(cache):
+    with open(CACHE_FILE, "w", encoding="utf-8") as file:
+        json.dump(cache, file, indent=4)
 
+
+cache = load_cache()
+
+
+def refresh_cache(base_currency):
+
+    current_time = time.time()
+
+    if "last_updated" in cache and current_time - cache["last_updated"] < CACHE_TTL:
+        return
+
+    print("Refreshing cache...")
     url = f"http://www.floatrates.com/daily/{base_currency}.json"
     response = requests.get(url)
 
     if response.status_code == 200:
-        exchange_data = response.json()
-        if target_currency in exchange_data:
-            rate = exchange_data[target_currency]["rate"]
-            cache[target_currency] = rate
-            return rate
-        else:
-            print(f"Exchange rate for {target_currency} is not available.")
-    else:
-        print("Failed to retrieve exchange rates.")
+        cache_data = response.json()
+        cache.clear()
+        cache["last_updated"] = current_time
 
+        for currency, data in cache_data.items():
+            cache[currency] = {"rate": data["rate"], "timestamp": current_time}
+
+        save_cache(cache)
+
+
+def get_exchange_rate(base_currency, target_currency):
+    refresh_cache(base_currency)
+
+    if target_currency in cache:
+        print("Checking the cache...")
+        print("It is in the cache!")
+        return cache[target_currency]["rate"]
+
+    print("Exchange rate not available.")
     return None
 
 
 def convert_currency():
     base_currency = input("Enter the currency you have: > ").lower()
+    refresh_cache(base_currency)
 
     while True:
         target_currency = input("Enter the currency to convert to (or press Enter to exit): > ").lower()
